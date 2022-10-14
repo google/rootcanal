8402b631056c2405512f5cab67d9de648f7550de
Grace Cham <hscham@google.com>
2022-10-14 11:59:24 +0900

    PDL: rename hci_packages enums
    * DirectAdvertisingAddressType::NO_ADDRESS{=>_PROVIDED}
    * PacketStatusFlag::NO_DATA{=>_RECEIVED}
    
    libchrome started to include libevent.h from r1033946
    (crrev.com/c/3811220) and hence indirectly include netdb.h, which
    defines macro NO_DATA and NO_ADDRESS. This causes compile error in floss
    when they are used in the enums.
    
    Use NO_ADDRESS_PROVIDED and NO_DATA_RECEIVED to be consistent with the
    spec (downloadable from
    https://www.bluetooth.com/specifications/specs/core-specification-5-3/,
    page 2272 and 1814 respectively)
    
    BUG: 253138188
    Tag: #floss
    Bug: 245578454
    Test: emerge-hatch libchrome floss
    Ignore-AOSP-First: Cherry-picked from AOSP
    Merged-In: I493017f800ffddab2a24d8cf3324d8e21e96aee0
    Change-Id: I493017f800ffddab2a24d8cf3324d8e21e96aee0

--- model/controller/link_layer_controller.cc
+++ model/controller/link_layer_controller.cc
@@ -1890,7 +1890,7 @@ void LinkLayerController::IncomingLeLegacyAdvertisingPdu(
     response.rssi_ = rssi;
     response.periodic_advertising_interval_ = 0;  // No periodic advertising.
     response.direct_address_type_ =
-        bluetooth::hci::DirectAdvertisingAddressType::NO_ADDRESS;
+        bluetooth::hci::DirectAdvertisingAddressType::NO_ADDRESS_PROVIDED;
     response.direct_address_ = Address::kEmpty;
     response.advertising_data_ = advertising_data;
 
