2b194c4c850658b5b5a557fa2681496c17705308
Henri Chataing <henrichataing@google.com>
2023-04-07 17:36:49 +0000

    RootCanal: Make own copy of hci_packets.pdl
    - Preparation for migrating RootCanal to github,
      removing the final dependencies to the bluetooth stack
    
    - Will allow editing of the hci_packets.pdl source
      without incurring large changes in gabeldorsche
    
    Bug: 256013143
    Test: mmm packages/modules/Bluetooth
    Change-Id: If79872ca4857786d8bd7a33f5674aa6b7ea15746

--- lmp/build.rs
+++ lmp/build.rs
@@ -18,24 +18,37 @@ use std::path::{Path, PathBuf};
 use std::process::Command;
 
 fn main() {
-    let packets_prebuilt = match env::var("LMP_PACKETS_PREBUILT") {
+    install_generated_module(
+        "lmp_packets.rs",
+        "LMP_PACKETS_PREBUILT",
+        &PathBuf::from("lmp_packets.pdl").canonicalize().unwrap(),
+    );
+    install_generated_module(
+        "hci_packets.rs",
+        "HCI_PACKETS_PREBUILT",
+        &PathBuf::from("../packets/hci/hci_packets.pdl").canonicalize().unwrap(),
+    );
+}
+
+fn install_generated_module(module_name: &str, prebuilt_var: &str, pdl_name: &PathBuf) {
+    let module_prebuilt = match env::var(prebuilt_var) {
         Ok(dir) => PathBuf::from(dir),
-        Err(_) => PathBuf::from("lmp_packets.rs"),
+        Err(_) => PathBuf::from(module_name),
     };
-    if Path::new(packets_prebuilt.as_os_str()).exists() {
+
+    if Path::new(module_prebuilt.as_os_str()).exists() {
         let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-        let outputted = out_dir.join("lmp_packets.rs");
         std::fs::copy(
-            packets_prebuilt.as_os_str().to_str().unwrap(),
-            out_dir.join(outputted.file_name().unwrap()).as_os_str().to_str().unwrap(),
+            module_prebuilt.as_os_str().to_str().unwrap(),
+            out_dir.join(module_name).as_os_str().to_str().unwrap(),
         )
         .unwrap();
     } else {
-        generate_packets();
+        generate_module(pdl_name);
     }
 }
 
-fn generate_packets() {
+fn generate_module(pdl_name: &PathBuf) {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     // Find the packetgen tool. Expecting it at CARGO_HOME/bin
@@ -46,17 +59,17 @@ fn generate_packets() {
 
     if !Path::new(packetgen.as_os_str()).exists() {
         panic!(
-            "Unable to locate bluetooth packet generator:{:?}",
+            "bluetooth_packetgen not found in the current environment: {:?}",
             packetgen.as_os_str().to_str().unwrap()
         );
     }
 
-    println!("cargo:rerun-if-changed=lmp_packets.pdl");
+    println!("cargo:rerun-if-changed={}", pdl_name.display());
     let output = Command::new(packetgen.as_os_str().to_str().unwrap())
         .arg("--out=".to_owned() + out_dir.as_os_str().to_str().unwrap())
-        .arg("--include=.")
+        .arg("--include=".to_owned() + pdl_name.parent().unwrap().as_os_str().to_str().unwrap())
         .arg("--rust")
-        .arg("lmp_packets.pdl")
+        .arg(pdl_name)
         .output()
         .unwrap();
 
@@ -66,4 +79,6 @@ fn generate_packets() {
         String::from_utf8_lossy(output.stdout.as_slice()),
         String::from_utf8_lossy(output.stderr.as_slice())
     );
+
+    assert!(output.status.success());
 }
--- lmp/src/packets.rs
+++ lmp/src/packets.rs
@@ -1,6 +1,96 @@
 pub mod hci {
-    pub use bt_packets::custom_types::*;
-    pub use bt_packets::hci::*;
+    #![allow(clippy::all)]
+    #![allow(unused)]
+    #![allow(missing_docs)]
+
+    pub const EMPTY_ADDRESS: Address = Address { bytes: [0x00, 0x00, 0x00, 0x00, 0x00, 0x00] };
+    pub const ANY_ADDRESS: Address = Address { bytes: [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF] };
+
+    /// A Bluetooth address
+    #[derive(Clone, Copy, Eq, PartialEq, Hash, Ord, PartialOrd, Debug)]
+    pub struct Address {
+        pub bytes: [u8; 6],
+    }
+
+    impl Address {
+        pub fn is_empty(&self) -> bool {
+            *self == EMPTY_ADDRESS
+        }
+    }
+
+    impl fmt::Display for Address {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(
+                f,
+                "{:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}",
+                self.bytes[5],
+                self.bytes[4],
+                self.bytes[3],
+                self.bytes[2],
+                self.bytes[1],
+                self.bytes[0]
+            )
+        }
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct InvalidAddressError;
+
+    impl TryFrom<&[u8]> for Address {
+        type Error = InvalidAddressError;
+
+        fn try_from(slice: &[u8]) -> std::result::Result<Self, Self::Error> {
+            match <[u8; 6]>::try_from(slice) {
+                Ok(bytes) => Ok(Self { bytes }),
+                Err(_) => Err(InvalidAddressError),
+            }
+        }
+    }
+
+    impl From<Address> for [u8; 6] {
+        fn from(addr: Address) -> [u8; 6] {
+            addr.bytes
+        }
+    }
+
+    #[derive(Clone, Eq, Copy, PartialEq, Hash, Ord, PartialOrd, Debug)]
+    pub struct ClassOfDevice {
+        pub bytes: [u8; 3],
+    }
+
+    impl fmt::Display for ClassOfDevice {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(
+                f,
+                "{:03X}-{:01X}-{:02X}",
+                ((self.bytes[2] as u16) << 4) | ((self.bytes[1] as u16) >> 4),
+                self.bytes[1] & 0x0F,
+                self.bytes[0]
+            )
+        }
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct InvalidClassOfDeviceError;
+
+    impl TryFrom<&[u8]> for ClassOfDevice {
+        type Error = InvalidClassOfDeviceError;
+
+        fn try_from(slice: &[u8]) -> std::result::Result<Self, Self::Error> {
+            match <[u8; 3]>::try_from(slice) {
+                Ok(bytes) => Ok(Self { bytes }),
+                Err(_) => Err(InvalidClassOfDeviceError),
+            }
+        }
+    }
+
+    impl From<ClassOfDevice> for [u8; 3] {
+        fn from(cod: ClassOfDevice) -> [u8; 3] {
+            cod.bytes
+        }
+    }
+
+    include!(concat!(env!("OUT_DIR"), "/hci_packets.rs"));
 
     pub fn command_remote_device_address(command: &CommandPacket) -> Option<Address> {
         #[allow(unused_imports)]
