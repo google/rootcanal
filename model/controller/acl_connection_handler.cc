miyu-xu/packages-modules-Bluetooth

RootCanal: Implement the LL phy update procedure
Implement the full semantic of the LE Set Phy and LE Set Default Phy
commands by performing the LL phy update procedure.

Bug: 275970864
Test: m root-canal
Change-Id: I144ec8c153e0f2a3d96bcc408a46f0d383077625

tools/rootcanal/model/controller/acl_connection_handler.cc
@@ -197,6 +197,11 @@ uint16_t AclConnectionHandler::GetHandleOnlyAddress(
   return kReservedHandle;
 }
 
+AclConnection& AclConnectionHandle::GetAclConnection(uint16_t handle) {
+  ASSERT_LOG(HasHandle(handle), "Unknown handle %d", handle);
+  return acl_connections_.at(handle);
+}
+
 AddressWithType AclConnectionHandler::GetAddress(uint16_t handle) const {
   ASSERT_LOG(HasHandle(handle), "Unknown handle %hd", handle);
   return acl_connections_.at(handle).GetAddress();

tools/rootcanal/model/controller/dual_mode_controller.cc
@@ -1815,7 +1815,7 @@ void DualModeController::LeSetDefaultPhy(CommandView command) {
   ErrorCode status = link_layer_controller_.LeSetDefaultPhy(
       command_view.GetAllPhysNoTransmitPreference(),
       command_view.GetAllPhysNoReceivePreference(),
-      command_view.GetTxPhysBitmask(), command_view.GetRxPhysBitmask());
+      command_view.GetTxPhys(), command_view.GetRxPhys());
   send_event_(bluetooth::hci::LeSetDefaultPhyCompleteBuilder::Create(
       kNumCommandPackets, status));
 }
@@ -1827,7 +1827,7 @@ void DualModeController::LeSetPhy(CommandView command) {
       command_view.GetConnectionHandle(),
       command_view.GetAllPhysNoTransmitPreference(),
       command_view.GetAllPhysNoReceivePreference(),
-      command_view.GetTxPhysBitmask(), command_view.GetRxPhysBitmask(),
+      command_view.GetTxPhys(), command_view.GetRxPhys(),
       command_view.GetPhyOptions());
   send_event_(bluetooth::hci::LeSetPhyStatusBuilder::Create(
       status, kNumCommandPackets));

google/rootcanal

model/controller/acl_connection_handler.cc
@@ -197,6 +197,11 @@ uint16_t AclConnectionHandler::GetHandleOnlyAddress(
   return kReservedHandle;
 }
 
+AclConnection& AclConnectionHandler::GetAclConnection(uint16_t handle) {
+  ASSERT_LOG(HasHandle(handle), "Unknown handle %d", handle);
+  return acl_connections_.at(handle);
+}
+
 AddressWithType AclConnectionHandler::GetAddress(uint16_t handle) const {
   ASSERT_LOG(HasHandle(handle), "Unknown handle %hd", handle);
   return acl_connections_.at(handle).GetAddress();

model/controller/dual_mode_controller.cc
@@ -1824,8 +1824,8 @@ void DualModeController::LeSetDefaultPhy(CommandView command) {
   ASSERT(command_view.IsValid());
   ErrorCode status = link_layer_controller_.LeSetDefaultPhy(
       command_view.GetAllPhysNoTransmitPreference(),
-      command_view.GetAllPhysNoReceivePreference(),
-      command_view.GetTxPhysBitmask(), command_view.GetRxPhysBitmask());
+      command_view.GetAllPhysNoReceivePreference(), command_view.GetTxPhys(),
+      command_view.GetRxPhys());
   send_event_(bluetooth::hci::LeSetDefaultPhyCompleteBuilder::Create(
       kNumCommandPackets, status));
 }
@@ -1836,9 +1836,8 @@ void DualModeController::LeSetPhy(CommandView command) {
   ErrorCode status = link_layer_controller_.LeSetPhy(
       command_view.GetConnectionHandle(),
       command_view.GetAllPhysNoTransmitPreference(),
-      command_view.GetAllPhysNoReceivePreference(),
-      command_view.GetTxPhysBitmask(), command_view.GetRxPhysBitmask(),
-      command_view.GetPhyOptions());
+      command_view.GetAllPhysNoReceivePreference(), command_view.GetTxPhys(),
+      command_view.GetRxPhys(), command_view.GetPhyOptions());
   send_event_(bluetooth::hci::LeSetPhyStatusBuilder::Create(
       status, kNumCommandPackets));
 }